6aaf1f46e8d0c078962aaec912f303a93fe4ac92
MrFZzzz <86085984+wjjdsgs123@users.noreply.github.com>
2022-12-10 15:19:57 +0800

    first test
    233

---
+++ springboot_demo1/src/main/java/com/mrfz/fzclass/Cat.java
@@ -0,0 +1,17 @@
+package com.mrfz.fzclass;
+
+public class Cat {
+    public String name;
+    public int id;
+
+    public Cat(){
+
+    }
+    public Cat(int id){
+        this.id=id;
+    }
+
+    public void say(){
+        System.out.println("哈哈哈哈");
+    }
+}
